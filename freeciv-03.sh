e641e30db6f1cbd0036a3167a9cb884c94cb96f9
James Robertson <23037938+jwrober@users.noreply.github.com>
2020-10-22 10:30:45 -0500

    Added some comments

--- freeciv-03.sh
+++ freeciv-03.sh
@@ -6,11 +6,13 @@ cd ~
 mkdir freeciv
 cd freeciv
 git clone https://github.com/freeciv/freeciv.git git
+### Uncomment master and S3_0 if you want to work on those brances
 #mkdir master
 #mkdir S3_0
 mkdir S2_6
 cd git
 git checkout S2_4
+### Uncomment master and S3_0 worktree lines if you uncommented the same above
 #git worktree add ../master/src master
 #git worktree add ../S3_0/src S3_0
 git worktree add ../S2_6/src S2_6
