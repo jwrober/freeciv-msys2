b13d939dd44fd930d08789f30514b360b65bc2fc
James Robertson <23037938+jwrober@users.noreply.github.com>
2021-01-12 14:08:51 -0600

    Update freeciv-03.sh
    Remove patch we no longer need with updated msys2 and code changes in freeciv 2.6.3

--- freeciv-03.sh
+++ freeciv-03.sh
@@ -17,10 +17,6 @@ git checkout S2_4
 #git worktree add ../S3_0/src S3_0
 git worktree add ../S2_6/src S2_6
 cd ~/freeciv/S2_6/src
-# We need a patch for S2_6 to work in latest Msys2 environment from http://forum.freeciv.org/f/viewtopic.php?f=14&t=91611
-wget https://www.hostedredmine.com/attachments/download/374154/0004-Msys2-Update-environment-to-200906-level.patch
-patch -p1 < 0004-Msys2-Update-environment-to-200906-level.patch
-rm 0004-Msys2-Update-environment-to-200906-level.patch
 ./autogen.sh --no-configure-run
 echo "#####"
 echo "# Done"
