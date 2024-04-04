488fb2c4927b06bfe6b4654a7bbb400741a04b32
James Robertson <23037938+jwrober@users.noreply.github.com>
2024-04-04 16:29:50 -0500

    Update freeciv-03.sh

--- freeciv-03.sh
+++ freeciv-03.sh
@@ -4,6 +4,8 @@ echo "# Setting up freeciv21 dev environment"
 echo "#####"
 cd ~
 git clone https://github.com/longturn/freeciv21.git freeciv21
+cd ~
+cd freeciv21
 echo "#####"
 echo "# Done"
 echo "#####"
