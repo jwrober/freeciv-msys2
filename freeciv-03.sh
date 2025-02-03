67a7a4714a26732272ca91c0ace6614631df0415
James Robertson <23037938+jwrober@users.noreply.github.com>
2025-02-03 16:47:45 -0600

    Update freeciv-03.sh
    Reduce number of steps

--- freeciv-03.sh
+++ freeciv-03.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 echo "#####"
-echo "# Setting up freeciv21 dev environment"
+echo "# Setting up a basic freeciv21 dev environment"
 echo "#####"
 cd ~
 git clone https://github.com/longturn/freeciv21.git freeciv21
 cd freeciv21
 cmake . -B build -G Ninja -DCMAKE_INSTALL_PREFIX=$PWD/build/install
 cmake --build build --target install
-cmake --build build --target package
-pip install -r docs/requirements.txt
-cmake --build build --target docs
