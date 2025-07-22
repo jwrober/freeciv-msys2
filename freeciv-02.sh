0cda304e89c467d390e64cc9bf5428298587e406
James Robertson <23037938+jwrober@users.noreply.github.com>
2025-07-21 19:09:30 -0500

    Update freeciv-02.sh - support for Qt6 and clang64

--- freeciv-02.sh
+++ freeciv-02.sh
@@ -9,68 +9,55 @@ echo "#####"
 echo "# Installing base non-arch specific tools"
 echo "#####"
 pacman -Su --noconfirm \
-    pkgconf \
     git \
-    automake \
-    libtool \
-    autoconf \
     patch \
-    ed \
+    pkgconf \
     python3 \
-    python-pip \
-    make \
-    clang
+    unzip \
+    zip  
 
 echo "#####"
-echo "# Installing mingw x86_64 arch specific tools"
+echo "# Installing clang x86_64 arch specific tools"
 echo "#####"
 echo "#####"
-echo "# Installing gcc and gdb"
-echo "#     Gives: readline, bzip2, and python3"
+echo "# Installing clang"
 echo "#####"
-pacman -Su --noconfirm mingw-w64-x86_64-gcc mingw-w64-x86_64-gdb 
+pacman -Su --noconfirm mingw-w64-clang-x86_64-clang mingw-w64-clang-x86_64-clang-tools-extra
 echo "#####"
-echo "# Installing curl"
+echo " Installing cmake, ninja, libunwind"
 echo "#####"
-pacman -Su --noconfirm mingw-w64-x86_64-curl
+pacman -Su --noconfirm mingw-w64-clang-x86_64-cmake mingw-w64-clang-x86_64-ninja mingw-w64-clang-x86_64-libunwind
+echo "#####"
+echo " Installing readline (for the server)"
+echo "#####"
+pacman -Su --noconfirm mingw-w64-clang-x86_64-readline
 echo "#####"
 echo "# Installing lua and tolua"
 echo "#####"
-pacman -Su --noconfirm mingw-w64-x86_64-lua mingw-w64-x86_64-tolua
+pacman -Su --noconfirm mingw-w64-clang-x86_64-lua mingw-w64-clang-x86_64-tolua
 echo "#####"
 echo "# Installing SDL2_mixer"
 echo "#####"
 pacman -Su --noconfirm mingw-w64-x86_64-SDL2_mixer
 echo "#####"
-echo " Installing cmake, ninja, libunwind"
+echo "# Installing Qt6"
 echo "#####"
-pacman -Su --noconfirm mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja mingw-w64-x86_64-libunwind
-echo "#####"
-echo "# Installing make"
-echo "#####"
-pacman -Su --noconfirm mingw-w64-x86_64-make
-echo "#####"
-echo "# Installing python3 pip and virtual env"
-echo "#####"
-pacman -Su --noconfirm mingw-w64-x86_64-python-pip mingw-w64-x86_64-python-virtualenv
-echo "#####"
-echo "# Installing Qt5"
-echo "#####"
-pacman -Su --noconfirm mingw-w64-x86_64-qt5 mingw-w64-x86_64-qt5-svg mingw-w64-x86_64-karchive-qt5
+pacman -Su --noconfirm mingw-w64-clang-x86_64-qt6-base mingw-w64-clang-x86_64-qt6-svg mingw-w64-clang-x86_64-qt6-multimedia mingw-w64-clang-x86_64-karchive
 echo "#####"
 echo "# Installing nsis"
 echo "#####"
-pacman -Su --noconfirm mingw-w64-x86_64-nsis
-
+pacman -Su --noconfirm mingw-w64-clang-x86_64-nsis
+echo "#####"
+echo "# Installing python"
+echo "#####"
+pacman -Su --noconfirm mingw-w64-clang-x86_64-python-pip mingw-w64-clang-x86_64-python-virtualenv
 echo "#####"
 echo "# Updating ~/.bash_profile"
 echo "#####"
-
 echo "" >> ~/.bash_profile
-echo 'PATH=/mingw64/bin:${PATH}' >> ~/.bash_profile
-echo 'PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/mingw64/lib/pkgconfig:/mingw64/share/pkgconfig' >> ~/.bash_profile
-echo "MSYSTEM=MINGW64" >> ~/.bash_profile
-
+echo 'PATH=/clang64/bin:${PATH}' >> ~/.bash_profile
+echo 'PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/clang64/lib/pkgconfig:/clang64/share/pkgconfig' >> ~/.bash_profile
+echo "MSYSTEM=CLANG64" >> ~/.bash_profile
 echo "#####"
 echo "# Updating MSYS2 base packages"
 echo "#####"
